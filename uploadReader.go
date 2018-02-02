1ecb23c1e3347880498af9254352448ce63d4aa2
Brice <Brice@iwdagency.com>
2018-02-01 19:56:02 -0600

    upload reader by line

--- uploadReader.go
+++ uploadReader.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
+	_ "fmt"
+	_ "io"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -25,30 +25,34 @@ func readUpload(uploadName string, lineChan *chan string) {
 	close(*lineChan)
 }
 
+func readUploadByLine(uploadName string, lineChan *chan string) {
+	upload, err := os.Open(uploadName)
+	defer upload.Close()
+	if err != nil {
+		panic(err)
+	}
 
-func readUploadByLine(uploadName string, lineChan *chan string){
-    upload, err := os.Open(uploadName)
-    defer upload.Close()
-    if err != nil {
-        panic(err)
-    }
+	// reader := bufio.NewReader(upload)
 
-    reader := bufio.NewReader(upload)
+	// for {
+	//     line, err := reader.ReadString('\n')
+	//     if err != nil {
+	//             if err == io.EOF {
+	//                 break
+	//             } else {
+	//                 fmt.Println(err)
+	//                 os.Exit(1)
+	//             }
+	//         }
+	//     *lineChan <- line
+	// }
 
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil {
-                if err == io.EOF {
-                    break
-                } else {
-                    fmt.Println(err)
-                    os.Exit(1)
-                }
-            }
-        *lineChan <- line
-    }
+	scanner := bufio.NewScanner(upload)
+	scanner.Buffer([]byte{}, 50)
 
+	for scanner.Scan() {
+		*lineChan <- scanner.Text()
+	}
 
-
-    defer close(*lineChan)
+	defer close(*lineChan)
 }
